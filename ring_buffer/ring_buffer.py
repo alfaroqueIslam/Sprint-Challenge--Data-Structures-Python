bb1a8ea9eda807e2378bc02eaa45d4f6b5ce3bf1
Alfaroque Islam <alfaroque5@gmail.com>
2020-09-11 14:01:09 -0400

    Sprint challenge

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,35 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.data = [None for i in range(capacity)]
+        self.old = self.data[0]
+        self.old1 = None
+  
     def append(self, item):
-        pass
+        lst = []
+        if self.data[0] == None:
+            self.data.pop(0)
+            self.data.append(item)
+            if self.data[0] != None:
+                self.old = self.data[0]
+            return
+        for n in self.data:
+            if n != None:
+                lst.append(n)
+        if len(lst) == self.capacity:
+            if self.data[0] == self.old:
+                self.data[0] = item
+                self.old1 = item
+                return
+            else:
+                x = lst.index(self.old1)
+                self.data[x+1] = item
+                self.old = item
 
     def get(self):
-        pass
+        lst = []
+        for n in self.data:
+            if n != None:
+                lst.append(n)
+
+        return lst
